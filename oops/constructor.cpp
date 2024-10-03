97be2caf13b79ec361128004360fa6cdc7c55fbd
DeepakDev2 <deepakkumar.nea10a@gmail.com>
2024-10-03 17:23:05 +0530

    Test

--- oops/constructor.cpp
+++ oops/constructor.cpp
@@ -1,33 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-class numbers{
+class numbers
+{
     int a;
     int b;
-    public:
-        numbers();
-        numbers(int x,int y );
 
-        void get_data(){
-            cout<<"The value of a and b is "<<a<<" "<<b<<endl;
-        }
+public:
+    numbers();
+    numbers(int x, int y);
+
+    void get_data()
+    {
+        cout << "The value of a and b is " << a << " " << b << endl;
+    }
 };
 
-numbers::numbers(){
-    a=1;
-    b=0;
+numbers::numbers()
+{
+    a = 1;
+    b = 0;
 }
-numbers::numbers(int x,int y){
-    a=x;
-    b=y;
+numbers::numbers(int x, int y)
+{
+    a = x;
+    b = y;
 }
-int main(){
-    numbers x;
-    numbers y(2,4);
+int main()
+{
+    numbers z;
+    numbers y(2, 4);
 
-    x.get_data();
+    z.get_data();
     y.get_data();
-    
+
     return 0;
 }
